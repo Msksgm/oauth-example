0eebbda4a4fedb6e35a7da0ca1e947c8eaed0867
Msksgm <msksgm3124@gmail.com>
2021-09-03 11:39:26 +0900

    add: HTTPリクエストからのOAuthトークンを解析する処理を追加

--- exercies/ch-4-ex-1/protectedResource.js
+++ exercies/ch-4-ex-1/protectedResource.js
@@ -26,6 +26,15 @@ var getAccessToken = function(req, res, next) {
 	/*
 	 * Scan for an access token on the incoming request.
 	 */
+	var inToken = null;
+	var auth = req.headers['authorization'];
+	if (auth && auth.toLowerCase().indexOf('bearer') === 0){
+		inToken = auth.slice('bearer '.length);
+	} else if (req.body && req.body.access_token) {
+		inToken = req.body.access_token;
+	} else if (req.query && req.query.access_token){
+		inToken = req.query.access_token;
+	}
 	
 };
 
