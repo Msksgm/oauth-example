8a409179c5dc0ac4b55c7d02826818028c43ebb0
Msksgm <msksgm3124@gmail.com>
2021-09-13 06:35:14 +0900

    add: jwtを生成、分析する処理を追加

--- exercies/ch-11-ex-1/protectedResource.js
+++ exercies/ch-11-ex-1/protectedResource.js
@@ -68,6 +68,31 @@ var getAccessToken = function(req, res, next) {
 	 * Parse and validate the JWT here
 	 */
 				
+	var tokenParts = inToken.split('.');
+	var payload = JSON.parse(base64url.decode(tokenParts[1]))
+	console.log('Payload', payload);
+	if (payload.iss == 'http://localhost:9001/') {
+		console.log('issuer OK');
+		if ((Array.isArray(payload.aud) && __.contains(payload.aud, 'http://localhost:9002/')) || 
+			payload.aud == 'http://localhost:9002/') {
+			console.log('Audience OK');
+			
+			var now = Math.floor(Date.now() / 1000);
+			
+			if (payload.iat <= now) {
+				console.log('issued-at OK');
+				if (payload.exp >= now) {
+					console.log('expiration OK');
+					
+					console.log('Token valid!');
+	
+					req.access_token = payload;
+					
+				}
+			}
+		}
+		
+	}
 	next();
 	return;
 	
